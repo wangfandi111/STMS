70309742454503c01fea7f2fa287389cddac2edd
lucienlgl <18813019599@163.com>
2019-12-18 18:03:41 +0800

    fix

--- src/main/java/cn/bjtu/stms/mapper/StuTaskMapper.java
+++ src/main/java/cn/bjtu/stms/mapper/StuTaskMapper.java
@@ -15,13 +15,13 @@ public interface StuTaskMapper extends Mapper<StuTask> {
     @Select("select * from stu_task where task_id = #{taskId} and stu_id = #{studentId}")
     StuTask getStuTask(@Param("taskId") Integer taskId, @Param("studentId") Integer studentId);
 
-    @Select("select distint(*) from stu_task where task_id = #{taskId} order by submit_time desc limit #{limit} offset #{offset}")
+    @Select("select * from stu_task where task_id = #{taskId} order by submit_time desc limit #{limit} offset #{offset}")
     List<StuTask> getStuTaskList(@Param("taskId") Integer taskId, @Param("limit") Integer limit, @Param("offset") Integer offset);
 
-    @Select("select ount(distint(stu_id)) from stu_task where task_id = #{taskId}")
+    @Select("select count(distinct(stu_id)) from stu_task where task_id = #{taskId}")
     Integer countStuTasks(@Param("taskId") Integer taskId);
 
-    @Select("select count(distint(stu_id)) from stu_task where task_id = #{taskId} and has_submitted = #{submitStatus}")
+    @Select("select count(distinct(stu_id)) from stu_task where task_id = #{taskId} and has_submitted = #{submitStatus}")
     Integer countStuTasksByStatus(@Param("taskId") Integer taskId, @Param("hasSubmitted") Integer submitStatus);
 
 }
